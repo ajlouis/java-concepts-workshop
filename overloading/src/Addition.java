72b58f8f0bf79f85b6464896575d4dba51cd2506
adzadogo <adzadogo@gmail.com>
2021-11-03 14:47:09 +0000

    initial commit

--- overloading/src/Addition.java
+++ overloading/src/Addition.java
@@ -6,5 +6,20 @@
 
 public class Addition {
 
+    public int add(int i, int i1) {
+        return i + i;
+    }
+
+    public int add(int i, int i1, int i2) {
+        return i + i1 + i2;
+    }
+
+    public float add(int i, int i1, float i2) {
+        return i + i1 + i2;
+    }
+
+    public String add(String i, String i1) {
+        return i + i1;
+    }
 }
 
